4e104e64bdfacdce8dfa5aa39252349ea4545548
seanEidrefNatividad <seaneidrefnatividad@gmail.com>
2025-10-31 12:05:20 +0800

    fix(actions): use supabase-js batch insert for saveQuestions

--- lib/actions.ts
+++ lib/actions.ts
@@ -9,20 +9,42 @@ export async function save(data:SaveForm) {
   saveQuestions(data.formId, data['data'])
 }
 
-export async function saveQuestions(formId: UniqueIdentifier, items: FormAction[]) {
+async function saveQuestions(formId: UniqueIdentifier, items: FormAction[]) {
   const supabase = await createClient();
+  const questions = items.map(i=>{
+    const {id, title, type} = i.data;
+    return {
+      id, title, type, 'form_id': formId
+    }
+  })
+  //  console.log(questions)
+  try {
+    const { error } = await supabase
+      .from('questions')
+      .insert(questions)
 
-  const { error } = await supabase.rpc('save_questions', {
-    p_form_id: formId,
-    p_questions: JSON.parse(JSON.stringify(items))
-  });
+    if (error) throw error;
 
-  if (error) {
-    console.error('RPC failed:', error);
-    throw error
-    // throw new Error('Could not save questions');
+  } catch (error: unknown) {
+    console.log(error instanceof Error ? error.message : "An error occurred");
   }
 }
+
+
+// export async function saveQuestions(formId: UniqueIdentifier, items: FormAction[]) {
+//   const supabase = await createClient();
+
+//   const { error } = await supabase.rpc('save_questions', {
+//     p_form_id: formId,
+//     // p_questions: [{"op":"addQuestion","data":{"id":'5c29ddf4-c6ad-4e65-912f-5ef7538fe48b',"title":"fasdfaesf", "type": "short-text"}},{"op":"addQuestion","data":{"id":'4016ff0d-403c-40cb-9695-ad8807c81ed1',"title":"fasdfaesf", "type": "short-text"}}]
+//   });
+
+//   if (error) {
+//     console.error('RPC failed:', error);
+//     throw error
+//     // throw new Error('Could not save questions');
+//   }
+// }
 
 const uid = () => crypto?.randomUUID?.();
 
