4bc2abb8a294f498723c82b155a7503270d3f696
seanEidrefNatividad <seaneidrefnatividad@gmail.com>
2025-11-21 11:04:15 +0800

    feat(pwa): add offline mode

---
+++ next.config.js
@@ -0,0 +1,26 @@
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require("next/constants");
+
+/** @type {(phase: string, defaultConfig: import("next").NextConfig) => Promise<import("next").NextConfig>} */
+module.exports = async (phase) => {
+  /** @type {import("next").NextConfig} */
+
+// Your current or future configuration 
+
+  const nextConfig = {
+    reactStrictMode: true
+  };
+
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withSerwist = (await import("@serwist/next")).default({
+      swSrc: "src/service-worker/app-worker.ts",
+      swDest: "public/sw.js",
+      reloadOnOnline: true,
+    });
+    return withSerwist(nextConfig);
+  }
+
+  return nextConfig;
+};
