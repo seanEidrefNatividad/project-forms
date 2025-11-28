51913a3292654113f058252d30d7845fcf7acf77
seanEidrefNatividad <seaneidrefnatividad@gmail.com>
2025-11-28 15:58:07 +0800

    style(dashboard): improve UI

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,11 +8,29 @@ export const revalidate = 0;
 export default function Page() {
   return (
     <>
-      <p>Dashboard Page</p>
-      <Suspense fallback={<FormsSkeleton/>}>
-        <FormTable query='' currentPage={1}/>
-      </Suspense>
-      <CreateForm/>
+      <div className='max-w-5xl mx-auto'>
+        <div>
+          <span className='text-xs'>Sean Eidref Natividad's</span>
+          <p className='text-lg'>
+            Forms | Dashboard
+          </p>
+        </div>
+
+        <Suspense fallback={<FormsSkeleton/>}>
+          <FormTable query='' currentPage={1}/>
+        </Suspense>
+
+        <div className='hidden md:block flex justify-end bg-background mt-6'>
+          <CreateForm/>
+        </div>
+
+        <div className='sm:hidden flex justify-center bg-background absolute inset-x-0 bottom-0 h-10'>
+          <div className='relative inset-x-0 bottom-7'>
+            <CreateForm/>
+          </div>
+        </div>
+
+      </div>
     </>
   );
 }
