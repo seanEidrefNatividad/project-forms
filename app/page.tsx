146835201fdcfdf34c706bbfd0f8152fbbb8b4f0
seanEidrefNatividad <seaneidrefnatividad@gmail.com>
2025-11-28 15:00:17 +0800

    chore(home page): backup the old default and make a new one

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import { DeployButton } from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import { AuthButton } from "@/components/auth-button";
-import { Hero } from "@/components/hero";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps";
-import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
-// import { redirect } from 'next/navigation';
+import TopNav from '@/components/ui/dashboard/top-nav';
 
 export default function Home() {
   // redirect('/dashboard');
@@ -15,38 +7,36 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-            <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
-              <div className="flex items-center gap-2">
-                <DeployButton />
-              </div>
-            </div>
-            {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
-          </div>
-        </nav>
+        <TopNav/>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          <Hero />
+          <div className="py-10 md:py-20">
+            <p className="text-lg md:text-2xl mx-auto mb-0 text-center">
+              Sean Eidref Natividad&apos;s 
+            </p>
+            <p className="text-6xl md:text-8xl !leading-tight mt-0 text-center">
+              PROJECTS
+            </p>
+          </div>
+
           <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Next steps</h2>
-            {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+            <Link href="/dashboard">
+              <div className='p-2 rounded border-[1px] border-foreground bg-card'>
+                <h2 className="font-medium text-lg mb-4">
+                  Forms &nbsp;
+                  <span className="text-xs text-muted-foreground">
+                    (click to view)
+                  </span>
+                </h2>
+                <p>A dynamic form-builder web app that lets you create custom forms, collect responses, and view analytics.</p>
+              </div>
+            </Link>
           </main>
         </div>
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
           <p>
-            Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a>
+            © 2025 Sean Eidref Natividad. All rights reserved.
           </p>
-          <ThemeSwitcher />
         </footer>
       </div>
     </main>
