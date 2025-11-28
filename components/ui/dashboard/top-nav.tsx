b8d3ea1765c5417892f9de43b20806967eaf2281
seanEidrefNatividad <seaneidrefnatividad@gmail.com>
2025-11-28 14:58:12 +0800

    style(top nav): improve UI

--- components/ui/dashboard/top-nav.tsx
+++ components/ui/dashboard/top-nav.tsx
@@ -1,14 +1,23 @@
 import { ThemeSwitcher } from "@/components/theme-switcher";
+import { AuthButton } from "@/components/auth-button";
+import Link from "next/link";
 
 export default function TopNav() {
   return (
-    <div className="p-1 md:p-2 w-full flex justify-between items-center ">
-      <div className="flex justify-start">
-        Logo
+    <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+      <div className="w-full flex justify-between items-center p-3 px-5 text-sm">
+        <div className="flex gap-5 items-center font-semibold">
+          <Link href={"/"}>Logo</Link>
+          <div className="flex items-center gap-2">
+          </div>
+        </div>
+        <div className="flex flex-direction-row justify-between">
+          <AuthButton />
+          <div className="ml-2">
+            < ThemeSwitcher />
+          </div>
+        </div>
       </div>
-      <div className="flex justify-end">
-        < ThemeSwitcher />
-      </div>
-    </div>
+    </nav>
   )
 }
